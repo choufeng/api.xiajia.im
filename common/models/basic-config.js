aa10cbf05237f1c23bbd925e2ca1c4c05e487fec
jon.xia <xiajia@vip.qq.com>
2018-08-28 16:58:11 +0800

    add suer-info

--- common/models/basic-config.js
+++ common/models/basic-config.js
@@ -1,15 +1,12 @@
-const datef = require('date-fns')
+'use strict';
+const addHours = require('date-fns/add_hours')
 module.exports = function(Basicconfig) {
-  // Basicconfig.beforeCreate = (next, m) => {
-  //   m.create = datef.addHours(new Date(), 8)
-  //   next()
-  // }
   Basicconfig.observe('before save', function updateTimestamp(ctx, next) {
     console.log(ctx)
     if (ctx.isNewInstance) {
-      ctx.instance.created_at = datef.addHours(new Date(), 8)
+      ctx.instance.created_at = addHours(new Date(), 8)
     } else {
-      ctx.data.updated_at = new datef.addHours(new Date(), 8)
+      ctx.data.updated_at = addHours(new Date(), 8)
     }
     next()
   })
